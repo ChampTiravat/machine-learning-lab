3f324d117eaf592b104e650743385fd90de12eaa
ChampTiravat <tiravat2016@gmail.com>
2024-06-16 01:25:45 +0700

    refactored

--- code/learn-tensorflow/simple_neural_network.py
+++ code/learn-tensorflow/simple_neural_network.py
@@ -3,11 +3,14 @@ import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 
-layer1 = Dense(units=1, input_shape=[1])
+model = Sequential([
+    Dense(units=1, input_shape=[1]),
+])
 
-model = Sequential([layer1])
-
-model.compile(optimizer='sgd', loss='mean_squared_error')
+model.compile(
+        optimizer='sgd',
+        loss='mean_squared_error',
+)
 
 x = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 y = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
